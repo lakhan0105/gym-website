469e552abbf90827ea50597d39273b582314c699
lakhan0105 <lakhan0105@gmail.com>
2024-11-09 15:13:34 +0530

    Fix the navlinks to hide in other routes

--- src/Components/Navbar/Navlinks.jsx
+++ src/Components/Navbar/Navlinks.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 const navLinks = [
   { linkName: "stories", link: "#stories" },
@@ -8,20 +9,33 @@ const navLinks = [
 ];
 
 function Navlinks({ extraClass, toggleSidebar }) {
-  return (
-    <ul className={`flex gap-8 capitalize ${extraClass}`}>
-      {navLinks.map((item) => {
-        const { linkName, link } = item;
-        return (
-          <li onClick={toggleSidebar} key={linkName}>
-            <a href={link} className="text-xl ">
-              {linkName}
-            </a>
-          </li>
-        );
-      })}
-    </ul>
-  );
+  const [showNavLinks, setShowNavLinks] = useState(true);
+  const location = useLocation();
+
+  useEffect(() => {
+    // check the path of the page, if it is other than "/" then setShowLinks(false)
+    if (location.pathname !== "/") {
+      setShowNavLinks(false);
+    }
+  }, [location]);
+
+  // if the showNavLink is true, oly then show the navlinks
+  if (showNavLinks) {
+    return (
+      <ul className={`flex gap-8 capitalize ${extraClass}`}>
+        {navLinks.map((item) => {
+          const { linkName, link } = item;
+          return (
+            <li onClick={toggleSidebar} key={linkName}>
+              <a href={link} className="text-xl ">
+                {linkName}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
 }
 
 export default Navlinks;

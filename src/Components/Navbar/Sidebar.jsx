69b7915d5c970066c2354649cced491c49fe6d8d
lakhan0105 <lakhan0105@gmail.com>
2024-11-09 15:54:16 +0530

    fixed the navbar styling in mobile view

--- src/Components/Navbar/Sidebar.jsx
+++ src/Components/Navbar/Sidebar.jsx
@@ -3,8 +3,13 @@ import Logo from "./Logo";
 import { FaXmark } from "react-icons/fa6";
 import Navlinks from "./Navlinks";
 import Sociallinks from "./Sociallinks";
+import { useMyContext } from "../Context/ContextProvider";
+import DashboardBtn from "./DashboardBtn";
+import LogoutBtn from "../Auth/LogoutBtn";
 
 function Sidebar({ toggleSidebar, isSidebarOpen }) {
+  const { currUser } = useMyContext();
+
   return (
     <div
       className={`fixed top-0 h-screen left-0 right-0 bg-white z-50 flex flex-col text-xl md:hidden ${
@@ -30,6 +35,14 @@ function Sidebar({ toggleSidebar, isSidebarOpen }) {
         extraClass={"justify-center pb-5"}
         toggleSidebar={toggleSidebar}
       />
+
+      {/* BUTTONS CONTAINER (admin buttons)  */}
+      {currUser && (
+        <div className="text-center md:hidden pb-5">
+          <DashboardBtn />
+          <LogoutBtn />
+        </div>
+      )}
     </div>
   );
 }
